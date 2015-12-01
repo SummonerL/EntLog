4c3fc64339bcd671afefa54d7f4ed6fa1dd7d13b
Elliotpwnz <jsimps72@CCIWD-120-308.its.uncc.edu>
2015-12-01 14:51:09 -0500

    basic filtering
    Added a way to filter by ent type

--- showLog/views.py
+++ showLog/views.py
@@ -7,7 +7,11 @@ from .models import Entry
 # Create your views here.
 def index(request):
     order_by = request.GET.get('order_by', 'name')
-    all_entries = Entry.objects.all().order_by(order_by)
+    filter_type = request.GET.get('filter', '')
+    if filter_type:
+        all_entries = Entry.objects.filter(ent_type=filter_type).order_by(order_by)
+    else:
+        all_entries = Entry.objects.all().order_by(order_by)
     template = loader.get_template('index.html')
     context = RequestContext(request, {
         'all_entries': all_entries,
