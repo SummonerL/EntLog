77a04b9dfaa82da47eb8042015d48fa1f1d8cb99
Elliotpwnz <jsimps72@CCIWD-120-310.its.uncc.edu>
2015-12-01 10:09:17 -0500

    initial commit
    first commit - includes project foundation and readme

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#EntLog - From movies, to books, to games - keep track of all the entertainment you've consumed.
---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jsimps72\entlog\entlog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
