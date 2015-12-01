69fd43d4ef2f92d82c4c747ccf7ad841b9b5ba46
Elliotpwnz <jsimps72@CCIWD-120-310.its.uncc.edu>
2015-12-01 12:27:44 -0500

    showLog view
    added a view to show the entry log - rendered to index.html

--- showLog/views.py
+++ showLog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Entry
 
 # Create your views here.
+def index(request):
+    order_by = request.GET.get('order_by', 'name')
+    all_entries = Entry.objects.all().order_by(order_by)
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'all_entries': all_entries,
+    })
+    return HttpResponse(template.render(context))
