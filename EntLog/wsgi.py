0adab6f027057cdeb8e125a945aca508414d1682
Elliot Simpson <Elliotsminion@gmail.com>
2015-12-01 20:07:17 -0500

    hope is all i have

--- EntLog/wsgi.py
+++ EntLog/wsgi.py
@@ -14,4 +14,7 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EntLog.settings")
 
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
